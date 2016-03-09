b8e6aefb603e51e57623a93ab9950af07fa593e1
czp3009 <czp3009@gmail.com>
2016-03-09 19:13:02 +0800

    挖坑中

--- src/server.c
+++ src/server.c
@@ -1,7 +1,27 @@
+#include <stdlib.h>
+#include <unistd.h>
 #include <stdio.h>
 
 int main()
 {
-    printf("hello world");
+    int pid;
+    int i;
+    char *exec_path = "/usr/local/bin/ss-server";
+    char *exec_arg[] = {"ss-server", "-s", "0.0.0.0", "-p", "10000", "-k", "1234", "-m", "aes-256-cfb", NULL};
+    char *exec_envp[] = {NULL};
+    for (i = 1; i <= 5; i++) {
+        pid = fork();
+        if (pid == 0) {
+            close(0);   //关闭输入输出
+            close(1);
+            close(2);
+            execve(exec_path, exec_arg, exec_envp);
+        }
+        else if (pid > 0) printf("已生成子进程 pid = %d\n", pid);
+        else printf("fork失败\n");
+    }
+
+    printf("sleeping....\n");
+    sleep(1000);
     return 0;
 }
